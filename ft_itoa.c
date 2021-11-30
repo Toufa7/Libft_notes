6bff6dbb3d2328f9af4c8028cd34586fb4c601e7
PØM$ <90482193+Toufa7@users.noreply.github.com>
2021-11-30 15:59:02 +0100

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -10,8 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-
-/* The first function helps to calculate the length of the int */
+// first function dedicated for calculating the lenght of the int 
 static int	intlen(int num)
 {
 	int	i;
@@ -26,7 +25,7 @@ static int	intlen(int num)
 		neg = 1;
 		num = num * -1;
 	}
-	while (num != 0)
+	while (num != 0) // so while the division of the number not equal 0 let's increase the index
 	{
 		num = num / 10;
 		i++;
@@ -42,7 +41,7 @@ char	*ft_itoa(int n)
 	char		*str;
 
 	sign = 0;
-	if (n < 0)
+	if (n < 0) // for checking if the number if negative then the sign will be 1 otherwise just skip and do not enter
 		sign = 1;
 	len = intlen(n);
 	str = malloc(sizeof(char) * len + 1);
@@ -51,14 +50,14 @@ char	*ft_itoa(int n)
 		return (0);
 	if (nbr < 0)
 		nbr = -nbr;
-	str[len--] = '\0';
-	while (len >= 0)
+	str[len--] = '\0'; // let's go the last index of the str and put a NULL\terminator (the same as str[len + 1] = because we start from zero) 
+	while (len >= 0) 
 	{
-		str[len] = nbr % 10 + '0';
+		str[len] = nbr % 10 + '0'; 
 		nbr = nbr / 10;
 		len--;
 	}
-	if (sign == 1)
+	if (sign == 1)  // if the sign declared before was 1 than let's put a '-' at the first of the string 
 		str[0] = '-';
 	return (str);
 }
