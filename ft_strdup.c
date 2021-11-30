518f5d96aa4000134a3202ea45670eece9995153
PØM$ <90482193+Toufa7@users.noreply.github.com>
2021-11-30 16:25:27 +0100

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+// Creating a new string (dup) that is identical to a string s1.
 
 char	*ft_strdup(const char *s1)
 {
@@ -19,14 +20,14 @@ char	*ft_strdup(const char *s1)
 	size_t	memory;
 
 	l = ft_strlen(s1);
-	memory = (sizeof(char) * l + 1);
+	memory = (sizeof(char) * l + 1); // allocating new memory for the duplicate string
 	dup = (char *)malloc(memory);
 	if (!(dup && s1))
 		return (NULL);
 	i = 0;
 	while (s1[i] != '\0')
 	{
-		dup[i] = s1[i];
+		dup[i] = s1[i]; // copying s1 as it is and past it on the dup and return it 
 		i++;
 	}
 	dup[i] = '\0';
